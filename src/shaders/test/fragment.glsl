2659a9a1d9cba1ba0555f748f5a1a46909216ebe
Florian <florianmtddev@gmail.com>
2025-09-28 15:47:36 +0200

    Patern 20

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -69,7 +69,12 @@ void main()
     // float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
 
     // Patern 19
-    float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    // float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+
+    // Patern 20
+    float square1 = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    float square2 = 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    float strength = square1 * square2;
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
