26d4a3a53154b66fbeacbe6b6cfb238fcdb3b347
Florian <florianmtddev@gmail.com>
2025-09-28 15:35:20 +0200

    Patern 14

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -37,9 +37,18 @@ void main()
     // float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
     // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
 
-    // Patern 12
-    float strength = step(0.4, mod(vUv.x * 10.0, 1.0));
-    strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+    // Patern 13
+    // float strength = step(0.4, mod(vUv.x * 10.0, 1.0));
+    // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    // Patern 14
+    float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
+    barX *= step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    float barY = step(0.8, mod(vUv.x * 10.0, 1.0));
+    barY *= step(0.4, mod(vUv.y * 10.0, 1.0));
+
+    float strength = barX + barY;
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
