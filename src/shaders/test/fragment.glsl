fdd74e1ae469f8155ab6178ca8d6ca8d5ea58c60
Florian <florianmtddev@gmail.com>
2025-09-28 15:17:34 +0200

    Patern 8

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -15,7 +15,12 @@ void main()
     // float strength = vUv.y * 10.0;
 
     // Patern 7
+    // float strength = mod(vUv.y * 10.0, 1.0);
+
+    // Patern 8
     float strength = mod(vUv.y * 10.0, 1.0);
+
+    strength = step(0.5, strength);
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
